2f448ef8fd8a14856457b2d6ac85d703ef037365
RansomBroker <yadisoke@gmail.com>
2024-03-29 23:13:14 +0700

    add header text

--- src/app/myjournal/page.js
+++ src/app/myjournal/page.js
@@ -1,6 +1,10 @@
 /** @format */
 const page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-2xl text-slate-500">My Journal</h1>
+    </div>
+  );
 };
 
 export default page;
